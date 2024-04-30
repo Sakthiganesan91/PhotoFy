03623d78f53d6cce9c2ce387ed29bf67ddc6e30a
Sakthiganesan <sakthiganesan158@gmail.com>
2024-04-30 22:54:44 +0530

    cleared web-hook error, started adding forms

--- src/app/(root)/transformations/add/[type]/page.tsx
+++ src/app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,32 @@
+import Header from "@/components/shared/Header";
+import TransformationForm from "@/components/shared/TransformationForm";
+import { getUserById } from "@/lib/actions/user.actions";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 import React from "react";
+import { transformationTypes } from "../../../../../../constants/index";
 
-const AddTransformationTypePage = () => {
-  return <div>AddTransformationPage</div>;
+const AddTransformationTypePage = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const transformation = transformationTypes[type];
+
+  const { userId } = auth();
+
+  if (!userId) redirect("/sign-in");
+  const user = await getUserById(userId);
+  return (
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+      <TransformationForm
+        action={"Add"}
+        data={null}
+        userId={user._id}
+        type={transformation.type as TransformationTypeKey}
+        creditBalance={user.creditBalance}
+      />
+    </>
+  );
 };
 
 export default AddTransformationTypePage;
