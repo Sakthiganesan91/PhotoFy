2f47681e5f79448319d9f0cbbd858d0426ac92a5
Sakthiganesan <sakthiganesan158@gmail.com>
2024-05-01 16:30:09 +0530

    added cloudinary features and db image upload

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,6 +3,7 @@
 import { type ClassValue, clsx } from "clsx";
 import qs from "qs";
 import { twMerge } from "tailwind-merge";
+import { aspectRatioOptions } from "../../constants/index";
 
 //import { aspectRatioOptions } from "@/constants";
 
@@ -93,21 +94,20 @@ export const debounce = (func: (...args: any[]) => void, delay: number) => {
   };
 };
 
-// GE IMAGE SIZE
-// export type AspectRatioKey = keyof typeof aspectRatioOptions;
-// export const getImageSize = (
-//   type: string,
-//   image: any,
-//   dimension: "width" | "height"
-// ): number => {
-//   if (type === "fill") {
-//     return (
-//       aspectRatioOptions[image.aspectRatio as AspectRatioKey]?.[dimension] ||
-//       1000
-//     );
-//   }
-//   return image?.[dimension] || 1000;
-// };
+export type AspectRatioKey = keyof typeof aspectRatioOptions;
+export const getImageSize = (
+  type: string,
+  image: any,
+  dimension: "width" | "height"
+): number => {
+  if (type === "fill") {
+    return (
+      aspectRatioOptions[image.aspectRatio as AspectRatioKey]?.[dimension] ||
+      1000
+    );
+  }
+  return image?.[dimension] || 1000;
+};
 
 // DOWNLOAD IMAGE
 export const download = (url: string, filename: string) => {
